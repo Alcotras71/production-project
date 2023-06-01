38a5b15d663edf68e6dbe0befbd93a6fb71df290
andrei alekseev <aimdragman2@yandex.ru>
2023-06-01 15:15:04 +0300

    add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,8 +6,6 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
-import './styles/index.scss';
-
 function App() {
     const { theme } = useTheme();
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -12,12 +13,26 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                className={cls.links}
+                theme={ButtonTheme.CLEAR_INVERTED}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                {/* eslint-disable-next-line */}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corporis culpa dolorem est, facere id in, minima obcaecati perferendis placeat recusandae reiciendis repellendus tempora veniam veritatis! Aliquam dolorem esse quas.
+            </Modal>
         </div>
     );
 };

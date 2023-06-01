60356d11572bef52fe872cea532eadc1e74e2498
andrei alekseev <aimdragman2@yandex.ru>
2023-06-01 08:45:08 +0300

    add storybook, add loki

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 
-import { useTranslation } from 'react-i18next';
 import cls from './NotFoundPage.module.scss';
 
 interface NotFoundPageProps {

74d1099ff14b460c729094b0830ce4ee1e4cc4b1
Andrey Alekseev <aimdragman2@yandex.ru>
2023-11-27 11:33:38 +0200

    configure babel for typescript, add fork ts checker, add custom babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,11 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+
     const fileLoader: webpack.RuleSetRule = {
         test: /\.(png|jpe?g|gif|woff|woff2)$/i,
         use: [
@@ -17,40 +20,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader: webpack.RuleSetRule = {
-        test: /\.(js|jsx|ts|tsx)/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        },
-                    ],
-                ],
-            },
-        },
-    };
-
+    const codeBabelLoader: webpack.RuleSetRule = buildBabelLoader({ ...options, isTsx: false });
+    const tsxCodeBabelLoader: webpack.RuleSetRule = buildBabelLoader({ ...options, isTsx: true });
     const cssLoader: webpack.RuleSetRule = buildCssLoader(isDev);
 
     // Если не используем тайпскрипт - нужен babel-loader
-    const typescriptLoader: webpack.RuleSetRule = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
+    // const typescriptLoader: webpack.RuleSetRule = {
+    //     test: /\.tsx?$/,
+    //     use: 'ts-loader',
+    //     exclude: /node_modules/,
+    // };
 
     return [
         fileLoader,
         svgLoader,
-        babelLoader,
-        typescriptLoader,
+        codeBabelLoader,
+        tsxCodeBabelLoader,
+        // typescriptLoader,
         cssLoader,
     ];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import CircularDepPlugin from 'circular-dependency-plugin';
 
@@ -33,6 +34,15 @@ export function buildPlugins({
         new CircularDepPlugin({
             exclude: /node_modules/,
             failOnError: true,
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            typescript: {
+                diagnosticOptions: {
+                    semantic: true,
+                    syntactic: true,
+                },
+                mode: 'write-references',
+            },
         }),
     ];
 

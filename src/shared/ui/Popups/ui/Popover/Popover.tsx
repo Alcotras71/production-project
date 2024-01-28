d6febd96539ea64f0b010f75ad54269e6f06dbaa
Andrey Alekseev <aimdragman2@yandex.ru>
2024-01-28 16:06:32 +0200

    add eslint plugin for unused imports

--- src/shared/ui/Popups/ui/Popover/Popover.tsx
+++ src/shared/ui/Popups/ui/Popover/Popover.tsx
@@ -1,5 +1,4 @@
-import { memo, ReactNode } from 'react';
-import { useTranslation } from 'react-i18next';
+import { ReactNode } from 'react';
 import { Popover as HPopover } from '@headlessui/react';
 
 import { classNames } from '@/shared/lib/helpers/classNames/classNames';

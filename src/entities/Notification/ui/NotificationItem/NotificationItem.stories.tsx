9efb41aabd889dec0f2acabecd832acc14a30915
Andrey Alekseev <aimdragman2@yandex.ru>
2024-02-01 19:16:04 +0200

    test ci pipeline

--- src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
@@ -13,5 +13,12 @@ export default meta;
 type Story = StoryObj<typeof NotificationItem>;
 
 export const Normal: Story = {
-    args: {},
+    args: {
+        notification: {
+            title: 'string',
+            id: '1',
+            href: 'href',
+            description: 'string',
+        },
+    },
 };
